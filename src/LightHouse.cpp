6cdf59e885bef673a3d23230d1f5b3380d6d6159
Stef-2 <capteinobvious@gmail.com>
2022-10-19 19:44:46 +0200

    Initial commit.

---
+++ src/LightHouse.cpp
@@ -0,0 +1,12 @@
+﻿// LightHouse.cpp : Defines the entry point for the application.
+//
+
+#include "LightHouse.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
