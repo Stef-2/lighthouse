e25ce02d791cf8dba81b05e177f46bbd6c855727
Stef-2 <capteinobvious@gmail.com>
2022-12-12 22:16:40 +0100

    working on vulkan rendering system

--- include/lighthouse/Renderer.hpp
+++ include/lighthouse/Renderer.hpp
@@ -8,6 +8,7 @@
 #include "version.hpp"
 #include "window.hpp"
 
+#include <iterator>
 #include <vector>
 
 namespace lh
@@ -20,12 +21,9 @@ namespace lh
         renderer(const window&, const engine_version& = engine_version::m_default,
                  const vulkan_version& = vulkan_version::m_default, bool use_validaiton_module = true);
 
-
-
     private:
         // optional module used for vulkan debugging
-        struct validation_module
-        {
+        struct validation_module {
             auto required_validation_layers() -> vk_string;
             auto supported_validation_layers() -> std::vector<vk::LayerProperties>;
             auto assert_required_validation_layers() -> bool;
@@ -35,23 +33,32 @@ namespace lh
                                                              const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                                              void* pUserData) -> VkBool32;
 
+            vk::DebugUtilsMessengerCreateInfoEXT m_debug_info {
+                .messageSeverity = vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose,
+                .messageType = vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral,
+                .pfnUserCallback = debug_callback};
+
             vk::raii::DebugUtilsMessengerEXT m_debug_messenger {nullptr};
-            vk_string m_required_validation_layers = {"VK_LAYER_KHRONOS_validation"};
+            vk_string m_required_validation_layers {"VK_LAYER_KHRONOS_validation"};
         };
 
-        struct extension_module
-        {
+        struct extension_module {
             auto required_extensions() -> vk_string;
             auto supported_extensions() -> std::vector<vk::ExtensionProperties>;
             auto assert_required_extensions() -> bool;
 
-            vk_string m_required_extensions = {"VK_EXT_debug_utils"};
+            vk_string m_required_extensions {"VK_EXT_debug_utils"};
         };
 
         auto create_instance(const window&, const engine_version& = engine_version::m_default,
-                             const vulkan_version& = vulkan_version::m_default, bool use_validaiton_module = true) -> vk::raii::Instance;
+                             const vulkan_version& = vulkan_version::m_default, bool use_validaiton_module = true)
+            -> vk::raii::Instance;
 
- 
+        auto get_queue_family_index() -> uint32_t;
+        auto get_physical_device_extensions() -> vk_string;
+
+        auto create_device() -> vk::raii::Device;
+        auto create_command_buffer() -> vk::raii::CommandBuffer;
 
         vulkan_version m_version;
 
@@ -59,8 +66,7 @@ namespace lh
         extension_module m_extension_module {};
 
         vk::raii::Instance m_instance {nullptr};
-        vk::raii::PhysicalDevices m_devices {nullptr};
-        vk::raii::SurfaceKHR m_surface {nullptr};
-        
+        vk::raii::Device m_device {nullptr};
+        vk::raii::CommandBuffer m_command_buffer {nullptr};
     };
 }
--- src/lighthouse/Renderer.cpp
+++ src/lighthouse/Renderer.cpp
@@ -4,8 +4,14 @@ lh::renderer::renderer(const window& window, const engine_version& engine_versio
                        bool use_validation_module)
     : m_version(vulkan_version),
       m_validation_module(use_validation_module ? validation_module {} : decltype(m_validation_module) {std::nullopt}),
-      m_instance(create_instance(window, engine_version, vulkan_version, use_validation_module))
+      m_instance(create_instance(window, engine_version, vulkan_version, use_validation_module)),
+      m_device {create_device()},
+      m_command_buffer {create_command_buffer()}
 {
+    if (use_validation_module)
+    {
+        m_validation_module->m_debug_messenger = {m_instance, m_validation_module->m_debug_info};
+    }
 }
 
 auto lh::renderer::extension_module::assert_required_extensions() -> bool
@@ -92,7 +98,7 @@ auto lh::renderer::extension_module::required_extensions() -> vk_string
     auto num_extensions = uint32_t {0};
     auto glfw_extensions = vkfw::getRequiredInstanceExtensions(&num_extensions);
 
-    auto combined_extensions = std::vector<const char*> {glfw_extensions, glfw_extensions + num_extensions};
+    auto combined_extensions = vk_string {glfw_extensions, glfw_extensions + num_extensions};
     combined_extensions.insert(combined_extensions.end(), m_required_extensions.begin(), m_required_extensions.end());
 
     return combined_extensions;
@@ -109,7 +115,6 @@ auto lh::renderer::create_instance(const window& window, const engine_version& e
 {
     auto app_info = vk::ApplicationInfo {};
     auto instance_info = vk::InstanceCreateInfo {};
-    auto debug_info = vk::DebugUtilsMessengerCreateInfoEXT {};
     auto validation_module = true;
 
     // configure application info
@@ -143,9 +148,86 @@ auto lh::renderer::create_instance(const window& window, const engine_version& e
     return {{}, instance_info};
 }
 
+auto lh::renderer::get_queue_family_index() -> uint32_t
+{
+    const auto physical_device = vk::raii::PhysicalDevices(m_instance).front();
+    const auto queue_family_properties = physical_device.getQueueFamilyProperties();
+
+    // get the first index into queue_family_properties which supports graphics
+    std::vector<vk::QueueFamilyProperties>::const_iterator queue_family_property = std::find_if(
+        queue_family_properties.begin(), queue_family_properties.end(),
+        [](vk::QueueFamilyProperties const& qfp) { return qfp.queueFlags & vk::QueueFlagBits::eGraphics; });
+
+    // if none of the available devices support the queue family, terminate
+    if (queue_family_property == queue_family_properties.end())
+        output::fatal() << "this system does not support the required vulkan components";
+
+    return static_cast<uint32_t>(std::distance(queue_family_properties.begin(), queue_family_property));
+}
+
+auto lh::renderer::get_physical_device_extensions() -> vk_string
+{
+    const auto physical_extensions = vk::raii::PhysicalDevices(m_instance).front().enumerateDeviceExtensionProperties();
+    auto physical_extension_names = vk_string {};
+    
+    std::ranges::for_each(physical_extensions.begin(), physical_extensions.end(),
+                          [&physical_extension_names](auto& ext)
+                          {
+                              physical_extension_names.push_back(ext.extensionName);
+                          });
+
+
+    return physical_extension_names;
+}
+
+auto lh::renderer::create_device() -> vk::raii::Device
+{
+    const auto physical_device = vk::raii::PhysicalDevices(m_instance).front();
+
+    const auto features_1 = physical_device.getFeatures();
+    const auto features_2 = physical_device.getFeatures2();
+
+    const auto physical_extensions = get_physical_device_extensions();/*
+    for (const auto& e : physical_extensions)
+    {
+        std::cout << e << '\n';
+    }*/
+    //std::cout << physical_extensions.data();
+    const auto queue_priority = 0.0f;
+    auto device_queue_info = vk::DeviceQueueCreateInfo {
+        .queueFamilyIndex = get_queue_family_index(),
+        .queueCount = 1,
+        .pQueuePriorities = &queue_priority,
+    };
+    std::cout << output::fatal();
+    auto device_info = vk::DeviceCreateInfo {};
+    device_info.queueCreateInfoCount = 1;
+    device_info.pQueueCreateInfos = &device_queue_info;
+    device_info.pEnabledFeatures = &features_1;
+
+    return {physical_device, device_info};
+}
+
+auto lh::renderer::create_command_buffer() -> vk::raii::CommandBuffer
+{
+    // create a command pool to allocate a command buffer from
+    auto command_pool_info = vk::CommandPoolCreateInfo {.queueFamilyIndex = get_queue_family_index()};
+    auto command_pool = vk::raii::CommandPool {m_device, command_pool_info};
+
+    // allocate a CommandBuffer from the CommandPool
+    auto command_buffer_allocate_info = vk::CommandBufferAllocateInfo {
+        .commandPool = *command_pool, .level = vk::CommandBufferLevel::ePrimary, .commandBufferCount = 1};
+
+    vk::raii::CommandBuffer command_buffer =
+        std::move(vk::raii::CommandBuffers(m_device, command_buffer_allocate_info).front());
+
+    return command_buffer;
+}
+
 VKAPI_ATTR auto VKAPI_CALL lh::renderer::validation_module::debug_callback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
     const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) -> VkBool32
 {
-    return false;
+    std::cout << "FUCK YOUR RETARDED MOTHER" << *pCallbackData->pMessage;
+    return true;
 }
