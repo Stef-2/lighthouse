4a639cbb4580f065201042b1e3032f09817713b1
Stef-2 <capteinobvious@gmail.com>
2023-03-25 20:30:23 +0100

    work on renderer

--- include/lighthouse/node.hpp
+++ include/lighthouse/node.hpp
@@ -1,63 +1,63 @@
 #pragma once
 
-#include "mat4x4.hpp"
+#include "vulkan/glm/mat4x4.hpp"
 
 #include <functional>
 #include <vector>
 
 namespace lh
 {
-    class node
-    {
-    public:
-        // internal transformation type
-        using transformation_t = glm::mat4x4;
+	class node
+	{
+	public:
+		// internal transformation type
+		using transformation_t = glm::mat4x4;
 
-        // mode that describes what happens to our relatives after we die
-        enum class destruction_mode
-        {
-            // connect out children with our parent
-            collapse,
-            // connect our children with the world node
-            orphanage
-        };
+		// mode that describes what happens to our relatives after we die
+		enum class destruction_mode
+		{
+			// connect out children with our parent
+			collapse,
+			// connect our children with the world node
+			orphanage
+		};
 
-        node(node& parent = world_node,
-             transformation_t = transformation_t {1.0f},
-             destruction_mode = destruction_mode::collapse);
-        ~node();
+		node(node& parent = world_node,
+			 transformation_t = transformation_t {1.0f},
+			 destruction_mode = destruction_mode::collapse);
+		~node();
 
-        static auto get_world_node() -> node&;
+		static auto get_world_node() -> node&;
 
-        auto set_parent(node&) -> void;
-        auto get_parent() const -> node&;
+		auto set_parent(node&) -> void;
+		auto get_parent() const -> node&;
 
-        auto add_child(node&) -> void;
-        auto remove_child(node&) -> void;
-        auto get_children() -> std::vector<node*>&;
+		auto add_child(node&) -> void;
+		auto remove_child(node&) -> void;
+		auto get_children() -> std::vector<node*>&;
 
-        auto is_ancestor_of(const node&) -> bool;
-        auto is_descendent_of(const node&) const -> bool;
-        auto is_sibling_of(const node&) const -> bool;
+		auto is_ancestor_of(const node&) -> bool;
+		auto is_descendent_of(const node&) const -> bool;
+		auto is_sibling_of(const node&) const -> bool;
 
-        auto operator==(const node&) const -> bool;
+		auto operator==(const node&) const -> bool;
 
-        auto set_local_transformation(const transformation_t&) -> void;
-        auto get_local_transformation() const -> transformation_t;
-        auto get_global_transformation() const -> transformation_t;
+		auto set_local_transformation(const transformation_t&) -> void;
+		auto get_local_transformation() const -> transformation_t;
+		auto get_global_transformation() const -> transformation_t;
 
-    private:
-        // remove ourselves from our current parents children list
-        // this must be followed with acquisition of a new parent, unless called from the destructor
-        auto get_disowned() const -> void;
+	private:
+		// remove ourselves from our current parents children list
+		// this must be followed with acquisition of a new parent, unless called from the destructor
+		auto get_disowned() const -> void;
 
-        node* m_parent;
-        std::vector<node*> m_children;
+		node* m_parent;
+		std::vector<node*> m_children;
 
-        transformation_t m_transformation;
+		transformation_t m_transformation;
 
-        destruction_mode m_destruction_mode;
+		destruction_mode m_destruction_mode;
 
-        static node world_node;
-    };
+		static node world_node;
+	};
 }
--- include/lighthouse/string.hpp
+++ include/lighthouse/string.hpp
@@ -1,6 +1,13 @@
 #pragma once
-// #include "vulkan/glm/gtx/string_cast.hpp"
-// #include "vulkan/vulkan_to_string.hpp"
+
+#include "vulkan/glm/gtx/string_cast.hpp"
+
+#ifndef VULKAN_HPP_NO_TO_STRING
+#include "vulkan/vulkan.h"
+#include "vulkan/vulkan_enums.hpp"
+#include "vulkan/vulkan_to_string.hpp"
+#endif
+
 #include "vkfw.hpp"
 
 #include <iostream>
@@ -16,19 +23,19 @@ namespace lh
 		// concepts that implicitly or explicitly convert to std::string
 		template <typename T>
 		concept std_convertible = requires(T x) { std::to_string(x); };
-		// template <typename T>
-		// concept glm_convertible = requires(T x) { glm::detail::compute_to_string<T>::call(x); };
+		template <typename T>
+		concept glm_convertible = requires(T x) { glm::detail::compute_to_string<T>::call(x); };
 		template <typename T>
 		concept vkfw_convertible = requires(T x) { vkfw::to_string(x); };
 		template <typename T>
 		concept std_constructible = requires(T x) { string_t(x); } || std::convertible_to<T, string_t>;
-		// template <typename T>
-		// concept vulkan_convertible = requires(T x) { vk::to_string(x); };
+		template <typename T>
+		concept vulkan_convertible = requires(T x) { vk::to_string(x); };
 
 		// combined string convertible concepts
 		template <typename T>
-		concept string_convertible = std_convertible<T> /*|| glm_convertible<T>*/ || vkfw_convertible<T> ||
-									 std_constructible<T> /*|| vulkan_convertible<T>*/;
+		concept string_convertible = std_convertible<T> || glm_convertible<T> || vkfw_convertible<T> ||
+									 std_constructible<T> || vulkan_convertible<T>;
 
 		// concept of a container range holding string convertible types
 		template <typename T>
