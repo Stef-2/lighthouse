96f0b4d62d166d212b7bfb324148bc88c1f8b814
Stef-2 <capteinobvious@gmail.com>
2023-06-19 20:33:39 +0200

    work on renderer

--- include/lighthouse/vulkan/swapchain.hpp
+++ include/lighthouse/vulkan/swapchain.hpp
@@ -94,30 +94,15 @@ namespace lh
 		{
 			constexpr auto queue_ownership = uint32_t {0};
 
-			constexpr auto top_of_pipe = vk::PipelineStageFlagBits2::eTopOfPipe;
-			constexpr auto bottom_of_pipe = vk::PipelineStageFlagBits2::eBottomOfPipe;
-
 			constexpr auto undefined_layout = vk::ImageLayout::eUndefined;
 			constexpr auto present_layout = vk::ImageLayout::ePresentSrcKHR;
 
 			constexpr auto color_layout = vk::ImageLayout::eColorAttachmentOptimal;
 			constexpr auto depth_stencil_layout = vk::ImageLayout::eDepthStencilAttachmentOptimal;
 
-			constexpr auto color_aspect = vk::ImageAspectFlagBits::eColor;
-			constexpr auto depth_stencil_aspect = vk::ImageAspectFlagBits::eDepth | vk::ImageAspectFlagBits::eStencil;
-
-			constexpr auto color_access = vk::AccessFlags2 {vk::AccessFlagBits2::eColorAttachmentWrite};
-			constexpr auto depth_stencil_access = vk::AccessFlags2 {vk::AccessFlagBits2::eDepthStencilAttachmentWrite};
-
-			constexpr auto color_subresource_range = vk::ImageSubresourceRange {color_aspect, 0, 1, 0, 1};
-			constexpr auto depth_stencil_subresource_range =
-				vk::ImageSubresourceRange {depth_stencil_aspect, 0, 1, 0, 1};
-
-			constexpr auto color_output_stage = vk::PipelineStageFlagBits2::eColorAttachmentOutput;
-			constexpr auto depth_stencil_output_stage = vk::PipelineStageFlagBits2::eEarlyFragmentTests |
-														vk::PipelineStageFlagBits2::eLateFragmentTests;
-
-			constexpr auto pipeline_stage = (state == layout_state::rendering) ? top_of_pipe : bottom_of_pipe;
+			constexpr auto pipeline_stage = (state == layout_state::rendering)
+												? vk::PipelineStageFlagBits2::eTopOfPipe
+												: vk::PipelineStageFlagBits2::eBottomOfPipe;
 
 			constexpr auto color_transition_to_rendering = (state == layout_state::rendering) ? undefined_layout
 																							  : color_layout;
@@ -125,44 +110,34 @@ namespace lh
 																	   ? undefined_layout
 																	   : depth_stencil_layout;
 
-			constexpr auto color_transition_to_presentation = (state == layout_state::presentation) ? color_layout
-																									: present_layout;
+			constexpr auto color_transition_to_presentation = (state == layout_state::presentation) ? present_layout
+																									: color_layout;
 			constexpr auto depth_stencil_transition_to_presentation = (state == layout_state::presentation)
-																		  ? depth_stencil_layout
-																		  : present_layout;
+																		  ? present_layout
+																		  : depth_stencil_layout;
 
 			const auto barriers = std::array<vk::ImageMemoryBarrier2, 2> {
 				vk::ImageMemoryBarrier2 {pipeline_stage,
-										 color_access,
-										 color_output_stage,
-										 color_access,
+										 {},
+										 vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+										 vk::AccessFlagBits2::eColorAttachmentWrite,
 										 color_transition_to_rendering,
 										 color_transition_to_presentation,
 										 queue_ownership,
 										 queue_ownership,
 										 m_object.getImages()[m_current_image_index],
-										 color_subresource_range},
-				vk::ImageMemoryBarrier2 {pipeline_stage,
-										 depth_stencil_access,
-										 depth_stencil_output_stage,
-										 depth_stencil_access,
-										 depth_stencil_transition_to_rendering,
-										 depth_stencil_transition_to_presentation,
-										 queue_ownership,
-										 queue_ownership,
-										 **m_depth_stencil_buffer,
-										 depth_stencil_subresource_range}};
-
-			const auto depth_stencil_barrier = vk::ImageMemoryBarrier2 {pipeline_stage,
-																		depth_stencil_access,
-																		depth_stencil_output_stage,
-																		depth_stencil_access,
-																		depth_stencil_transition_to_rendering,
-																		depth_stencil_transition_to_presentation,
-																		queue_ownership,
-																		queue_ownership,
-																		**m_depth_stencil_buffer,
-																		depth_stencil_subresource_range};
+										 {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}},
+				vk::ImageMemoryBarrier2 {
+					pipeline_stage,
+					{},
+					vk::PipelineStageFlagBits2::eEarlyFragmentTests | vk::PipelineStageFlagBits2::eLateFragmentTests,
+					vk::AccessFlagBits2::eDepthStencilAttachmentWrite,
+					depth_stencil_transition_to_rendering,
+					depth_stencil_transition_to_presentation,
+					queue_ownership,
+					queue_ownership,
+					**m_depth_stencil_buffer,
+					{vk::ImageAspectFlagBits::eDepth | vk::ImageAspectFlagBits::eStencil, 0, 1, 0, 1}}};
 
 			const auto dependency_info = vk::DependencyInfo {{}, {}, {}, barriers};
 			command_buffer.pipelineBarrier2(dependency_info);
