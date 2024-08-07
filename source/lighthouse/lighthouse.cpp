1dd0f7acd64337925024395c1ab21f4a7d26d3eb
Stef-2 <capteinobvious@gmail.com>
2024-08-07 23:02:56 +0200

    work on engine

--- source/lighthouse/lighthouse.cpp
+++ source/lighthouse/lighthouse.cpp
@@ -2,12 +2,15 @@
 
 import engine;
 import window;
+import bounding_volume;
 
 import std;
 
 auto main() -> int
 {
-	auto engine = lh::engine {std::make_unique<lh::window>(lh::window::create_info {.m_resolution {1280, 720}})};
+	lh::geometry::octree o {{{0.0f, 0.0f, 0.0f}, {8.0f, 8.0f, 8.0f}}, 0.1};
 
-	engine.run();
+	// auto engine = lh::engine {std::make_unique<lh::window>(lh::window::create_info {.m_resolution {1280, 720}})};
+
+	// engine.run();
 }
